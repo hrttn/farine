7613703c32cd617899e79ff8e52bdb450f57a48f
hrttn <hrettien@gmail.com>
2024-02-03 19:11:12 +0100

    feat: :sparkles: add stepper

--- src/app/bake/page.tsx
+++ src/app/bake/page.tsx
@@ -1,11 +1,60 @@
-import { Box, Heading } from "@chakra-ui/react";
+"use client";
+import {
+  Box,
+  Heading,
+  Step,
+  StepDescription,
+  StepIcon,
+  StepIndicator,
+  StepNumber,
+  StepSeparator,
+  StepStatus,
+  StepTitle,
+  Stepper,
+  useSteps,
+} from "@chakra-ui/react";
+
+const steps = [
+  { title: "Levain", description: "Create your levain" },
+  { title: "Autolyse", description: "Mix water & flour" },
+  { title: "Bulk fermentation", description: "Stretch and fold" },
+  { title: "Divide & preshape", description: "Let it rest on the counter" },
+  { title: "Shaping", description: "Shape into a boule, batard, baguette" },
+  { title: "Proofing", description: "Let the gluten do the wonder" },
+  { title: "Baking", description: "Bake your bread" },
+];
 
 export default function BakePage() {
+  const { activeStep, setActiveStep } = useSteps({
+    index: 1,
+    count: steps.length,
+  });
+
   return (
     <Box>
       <Heading as="h1" size="2xl">
         Farine: make better bread
       </Heading>
+      <Stepper index={activeStep} orientation="vertical" colorScheme="teal">
+        {steps.map((step, index) => (
+          <Step key={index}>
+            <StepIndicator onClick={() => setActiveStep(index)}>
+              <StepStatus
+                complete={<StepIcon />}
+                incomplete={<StepNumber />}
+                active={<StepNumber />}
+              />
+            </StepIndicator>
+
+            <Box flexShrink="0">
+              <StepTitle>{step.title}</StepTitle>
+              <StepDescription>{step.description}</StepDescription>
+            </Box>
+
+            <StepSeparator />
+          </Step>
+        ))}
+      </Stepper>
     </Box>
   );
 }
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,21 @@
 "use client";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  /* colors: {
+    teal: "#688d95",
+    white: "#fefefe",
+    blue: "#7fbae6",
+    yellow: "#f2b705",
+    orange: "#f28705",
+    red: "#8c271e",
+  },*/
+  styles: {
+    global: {},
+  },
+});
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
